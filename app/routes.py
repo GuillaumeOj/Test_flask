03ab60f991f28d60003949421d1f61738f70161b
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-04-02 17:42:21 +0200

    Use templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,18 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello world!'
+    user = {'user_name': 'Guillaume'}
+    posts = [
+                {
+                    'author': {'user_name': 'Michel'},
+                    'body': 'Beautiful day in Portland'
+                },
+                {
+                    'author': {'user_name': 'Babeth'},
+                    'body': 'The Avengers\' movie was so cool!'
+                }]
+    return render_template('index.html', title='Home', user=user, posts=posts)
